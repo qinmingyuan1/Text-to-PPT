5f32732b1fc36ffc5217dcc3ba4a36b93e1d67a6
PolyFesneauLi <24063117g@connect.polyu.hk>
2024-10-22 21:18:45 +0800

    add warning for topic hide header optimize chat_history when topic is changed add popup window add streamlit_modal in requirements.txt optimize efficiency in progress bar complete comments

--- app.py
+++ app.py
@@ -7,20 +7,14 @@ import toml,json, requests
 
 import pandas as pd
 import docx2txt
-import fitz  # PyMuPDF
-from pdf2image import convert_from_bytes
-from PIL import Image, ImageDraw
-
-import tempfile
 import comtypes.client
 
-# from pptx import Presentation
-# from pdf2image import convert_from_path
-# from PIL import Image
-# import io
-# import os
-# from comtypes.client import CreateObject
+# popup modal
+from streamlit_modal import Modal
 
+#** debug 
+# import time
+# import tempfile
 
 #openai.api_key = os.getenv('OPENAI_API_KEY')  # Replace with your actual API key
 file_path = './credential.txt'
@@ -34,9 +28,8 @@ OPENROUTER_API_KEY = secrets['OPENROUTER']['OPENROUTER_API_KEY']
 TITLE_FONT_SIZE = Pt(30)
 SLIDE_FONT_SIZE = Pt(16)
 
-
-def generate_slide_titles(topic):
-    prompt = f"Generate 5 slide titles for the topic '{topic}'."
+def generate_slide_titles(MinSlide,MaxSlide,topic):
+    prompt = f"Generate slide titles for the topic '{topic}'. The number of slides should be between {MinSlide} and {MaxSlide}."
     msg = [
         {"role": "system", "content":prompt},
         {"role":"user", "content":topic}
@@ -49,14 +42,11 @@ def generate_slide_titles(topic):
             "model": "openai/gpt-4o-mini-2024-07-18"
         })
     )
-    #print(response.json())
     resp = response.json()['choices'][0]['message']['content'].split("\n")
-    #resp = response.json()['choices'][0]['message']
-    #print("resp",resp)
     return resp
 
-def generate_slide_content(slide_title):
-    prompt = f"Generate content for the slide: '{slide_title}'."
+def generate_slide_content(MaxLine,slide_title):
+    prompt = f"Generate content for the slide: '{slide_title}'.And limit the content every page to {MaxLine} lines."
     msg = [
         {"role": "system", "content":prompt},
         {"role":"user", "content":slide_title}
@@ -69,13 +59,10 @@ def generate_slide_content(slide_title):
             "model": "openai/gpt-4o-mini-2024-07-18"
         })
     )
-    #print(response.json())
     resp = response.json()['choices'][0]['message']['content']
-    #print("contentresp",resp)
-    #resp = response.json()['choices'][0]['message']
     return resp
 
-def create_presentation(topic, slide_titles, slide_contents):
+def create_presentation(topic, slide_titles, slide_contents)->str:
     prs = pptx.Presentation()
     slide_layout = prs.slide_layouts[1]
 
@@ -95,18 +82,33 @@ def create_presentation(topic, slide_titles, slide_contents):
                 text_frame = shape.text_frame
                 for paragraph in text_frame.paragraphs:
                     paragraph.font.size = SLIDE_FONT_SIZE
+    # make sure no invalid characters in filename
+    t = "".join([c for c in topic if c.isalpha() or c.isdigit() or c==' ']).rstrip()
+    t = t.replace(" ", "_")
+    prs.save(f"./{t}.pptx")
+    return f"./{t}.pptx"
 
-    prs.save(f"./{topic}_presentation.pptx")
-    
-def get_ppt_download_link(topic):
-    ppt_filename = f"./{topic}_presentation.pptx"
-
-    with open(ppt_filename, "rb") as file:
+# to call the function, use the following code
+# link = get_ppt_download_link(PATH_TO_PPT)
+# st.markdown(link, unsafe_allow_html=True)  
+# then you can have a hyperlink to download the file
+def get_ppt_download_link(path, filename="presentation.pptx"):
+    with open(path, "rb") as file:
         ppt_contents = file.read()
-
     b64_ppt = base64.b64encode(ppt_contents).decode()
-    return f'<a href="data:application/vnd.openxmlformats-officedocument.presentationml.presentation;base64,{b64_ppt}" download="{ppt_filename}">Download the PowerPoint Presentation</a>'
+    return f'<a href="data:application/vnd.openxmlformats-officedocument.presentationml.presentation;base64,{b64_ppt}" download="{path}",filename>Download the PowerPoint Presentation</a>'
 
+# return url for button download
+# to call the function, use the following code
+# url = get_ppt_download_url(PATH_TO_PPT,PPT_NAME)
+# st.link_button("Download Presentation", url)
+def get_ppt_download_url(path):
+    with open(path, "rb") as file:
+        ppt_contents = file.read()
+    b64_ppt = base64.b64encode(ppt_contents).decode()
+    return f'data:application/vnd.openxmlformats-officedocument.presentationml.presentation;base64,{b64_ppt}'
+
+# Convert PowerPoint file to PDF, need know the path 
 def ppt_to_pdf(ppt_file_path, pdf_file_path):
     # Ensure the file paths are absolute
     ppt_file_path = os.path.abspath(ppt_file_path)
@@ -124,10 +126,8 @@ def ppt_to_pdf(ppt_file_path, pdf_file_path):
     try:
         # Open the presentation
         presentation = powerpoint.Presentations.Open(ppt_file_path)
-
         # Save as PDF
         presentation.SaveAs(pdf_file_path, 32)  # 32 is the formatType for PDF
-
         # Close the presentation
         presentation.Close()
     except Exception as e:
@@ -137,50 +137,115 @@ def ppt_to_pdf(ppt_file_path, pdf_file_path):
         powerpoint.Quit()
         comtypes.CoUninitialize()
 
+# use to display pdf file in web from path
 def empdf(pdf_file_path):
     with open(pdf_file_path, "rb") as f:
         base64_pdf = base64.b64encode(f.read()).decode('utf-8')
-    pdf_display = f'<iframe src="data:application/pdf;base64,{base64_pdf}" width="100%" height="800px"></iframe>'
+    pdf_display = f'<iframe src="data:application/pdf;base64,{base64_pdf}" width="100%" height="800px" title="output_pdf"></iframe> '
     return pdf_display
-
+# use to display pdf file in web from file
 def embed_pdf(file):
     base64_pdf = base64.b64encode(file.read()).decode('utf-8')
     pdf_display = f'<iframe src="data:application/pdf;base64,{base64_pdf}" width="100%" height="800px"></iframe>'
     return pdf_display
 
 def main():
-    st.markdown(
-        """
-        <style>
-        .main .block-container {
-            width: 100% !important;
-            max-width: 100% !important;
-            padding-left: 2rem;
-            padding-right: 2rem;
+    # web page title
+    st.set_page_config(page_title="PowerPoint Presentation Generator",layout="wide")
+    st.title("PowerPoint Presentation Generator")
+    # Hide the menu
+    st.markdown("""
+    <style>
+        .reportview-container {
+            margin-top: -2em;
         }
-        </style>
-    """,
-    unsafe_allow_html=True
-    )
-    st.title("PowerPoint Presentation Generator with GPT-3.5-turbo")
-    # Create two columns
-    col1, col2 = st.columns(2)
-    txt = None
-    wd = None
-    df = None
-    pf = None
-    md = None
-    ppt = None
-    with col2:
-        topic = st.text_input("Enter the topic for your presentation:")
-        preferences = st.text_input("Enter your preferences for the presentation:")
-        uploaded_file = st.file_uploader("Choose a file",type=['md','docx','csv','pdf','pptx','txt'])
-        chat = st.text_area("Chat with the AI")
-        GenerateButton = st.button("Generate Presentation")
-        ShareButton = st.button("Share Presentation")
+        #MainMenu {visibility: hidden;}
+        .stDeployButton {display:none;}
+        header {visibility: hidden;}
+        footer {visibility: hidden;}
+        #stDecoration {display:none;}
+    </style>
+""", unsafe_allow_html=True)
+    # Create two columns with different proportions
+    col1, col2 = st.columns([3,2])
+    txt = wd = df = None
+    pf = md = ppt = None
+
+    # defoult values
+    if "MinSlide" not in st.session_state:
+        st.session_state.MinSlide = 3
+    if "MaxSlide" not in st.session_state:
+        st.session_state.MaxSlide = 10
+    if "MaxLine" not in st.session_state:
+        st.session_state.MaxLine = 10
+    if "chat_history" not in st.session_state:
+        st.session_state.chat_history = []
+    if "user_input" not in st.session_state:
+        st.session_state.user_input = ""
+    if "outputpath" not in st.session_state:
+        st.session_state.outputpath = None
+    if "generatedpath" not in st.session_state:
+        st.session_state.generatedpath = None
+
+    # popup window when generating presentation
+    my_modal = Modal(title="Running Status", key="modal_key", max_width=600)
+
+    with col2: # Right column
+        topic = st.text_input("Enter the topic for your presentation:",help="mandatory")
+        if topic == "":
+            st.warning("Please enter a topic for your presentation.")
+        # Create a row with three columns for the number inputs
+        col_min, col_max, col_line = st.columns(3)
+        with col_min:
+            st.session_state.MinSlide = st.number_input("Min page num:", min_value=0, max_value=100, value=3, step=1)
+        with col_max:
+            st.session_state.MaxSlide = st.number_input("Max page num:", min_value=0, max_value=100, value=10, step=1)
+        with col_line:
+            st.session_state.MaxLine = st.number_input("MaxLine per page:", min_value=0, max_value=20, value=10, step=1)
+        uploaded_file = st.file_uploader("Choose a file",type=['md','docx','csv','txt'])
+        # New input area for user
+        user_input = st.chat_input("If you have more requirements to state:")
+        col_gen, col_exit = st.columns([1,1])
+        with col_gen:
+            GenerateButton = st.button("Generate Presentation")
+        with col_exit:
+            # the last ppt file in local path will be removed
+            Exit = st.button("Exit Application And Clear Cache",type="primary")
+        if st.session_state.generatedpath is not None:
+            url = get_ppt_download_url(st.session_state.generatedpath)
+            # st.link_button("Download Presentation", url) # not working
+            # get the filename from the path after /
+            filename = st.session_state.generatedpath.split("/")[-1]
+            # style the download button
+            st.markdown(f"""
+            <a href="{url}" download={filename} style="
+                display: inline-block;
+                padding: 0.5em 1em;
+                font-size: 1em;
+                color: black;
+                background-color: white;
+                border: 1px solid green;
+                border-radius: 0.25em;
+                text-decoration: none;
+                cursor: pointer;
+            ">Download Presentation</a>
+        """, unsafe_allow_html=True)
+        if Exit:
+            os.remove(st.session_state.generatedpath)  #clear cache file
+            st.stop()
+            # shut down the app
+
         # Add checkboxes for user to select options
         show_input_file = st.checkbox("Display Input File", value=True)
         show_output_preview = st.checkbox("Display Output Preview", value=True)
+        st.markdown("##### Chat History")
+        messages = st.container(height=300, key="chat_history_display")
+        # if topic changes , we have to clear the chat history
+        # first cell store current topic
+        if st.session_state.chat_history != [] and st.session_state.chat_history[0] != "":
+            for message in st.session_state.chat_history[1:]:
+                messages.chat_message("user").write(message)
+        # according to the file type, pass the content to variables
         if uploaded_file is not None:
             if uploaded_file.type == 'text/csv':
                 df = pd.read_csv(uploaded_file)
@@ -190,14 +255,27 @@ def main():
                 wd = docx2txt.process(uploaded_file)
             elif uploaded_file.type == 'text/plain':
                 txt = uploaded_file.getvalue().decode("utf-8")
-            elif uploaded_file.type == 'application/pdf':
-                pf = uploaded_file
-            elif uploaded_file.type == 'application/vnd.openxmlformats-officedocument.presentationml.presentation':
-                ppt = uploaded_file
+            #** debug
+            # elif uploaded_file.type == 'application/pdf':
+            #     pf = uploaded_file
+            # elif uploaded_file.type == 'application/vnd.openxmlformats-officedocument.presentationml.presentation':
+            #     ppt = uploaded_file
             else:
                 st.write("File not supported")
-    with col1:
-        if show_input_file:
+        
+    with col1: # Left column
+        if show_output_preview: # Display the output ppt preview
+            if st.session_state.outputpath is not None:
+                st.markdown("## Output Preview")
+                ppt_to_pdf(st.session_state.outputpath, st.session_state.outputpath.replace(".pptx", ".pdf"))
+                st.markdown(empdf(st.session_state.outputpath.replace(".pptx", ".pdf")), unsafe_allow_html=True)
+                os.remove(st.session_state.outputpath.replace(".pptx", ".pdf"))
+                st.session_state.generatedpath = st.session_state.outputpath
+                st.session_state.outputpath = None
+
+        if show_input_file: # Display the input file preview
+            if (txt is not None) or (wd is not None) or (df is not None) or (md is not None):
+                st.markdown("## Input Preview")
             if txt is not None:
                 st.text_area("TXT File Content", txt, height=400)
             if wd is not None:
@@ -206,36 +284,68 @@ def main():
                 st.write(df)
             if md is not None:
                 st.markdown(md)
-            if pf is not None:
-                st.markdown(embed_pdf(pf), unsafe_allow_html=True)
-            if ppt is not None:
-                # Save the uploaded file to a temporary location
-                with tempfile.NamedTemporaryFile(delete=False, suffix=".pptx") as tmp_ppt:
-                    tmp_ppt.write(ppt.getbuffer())
-                    tmp_ppt_path = tmp_ppt.name
-                    tmp_pdf_path = tmp_ppt_path.replace(".pptx", ".pdf")
-                # Convert the PowerPoint file to pdf
-                ppt_to_pdf(tmp_ppt_path, tmp_pdf_path)
-                # Display the PDF file
-                st.markdown(empdf(tmp_pdf_path), unsafe_allow_html=True)
-                # Clean up the temporary PPTX file
-                print("tmp_ppt_path--------------",tmp_ppt_path)
-                os.remove(tmp_ppt_path)
-                os.remove(tmp_pdf_path) 
-    if GenerateButton and topic:
-        # st.info("Generating presentation... Please wait.")
-        # slide_titles = generate_slide_titles(topic)
-        # filtered_slide_titles= [item for item in slide_titles if item.strip() != '']
-        # #print("Slide Title: ", filtered_slide_titles)
+            #** debug
+            # if pf is not None:
+            #     st.markdown(embed_pdf(pf), unsafe_allow_html=True)
+            # if ppt is not None:
+            #     # Save the uploaded file to a temporary location
+            #     with tempfile.NamedTemporaryFile(delete=False, suffix=".pptx") as tmp_ppt:
+            #         tmp_ppt.write(ppt.getbuffer())
+            #         tmp_ppt_path = tmp_ppt.name
+            #         tmp_pdf_path = tmp_ppt_path.replace(".pptx", ".pdf")
+            #     # Convert the PowerPoint file to pdf
+            #     ppt_to_pdf(tmp_ppt_path, tmp_pdf_path)
+            #     # Display the PDF file
+            #     st.markdown(empdf(tmp_pdf_path), unsafe_allow_html=True)
+            #     # Clean up the temporary PPTX file
+            #     os.remove(tmp_ppt_path)
+            #     os.remove(tmp_pdf_path) 
+            
 
-        # slide_contents = [generate_slide_content(title) for title in filtered_slide_titles]
-        # #print("Slide Contents: ", slide_contents)
-        # create_presentation(topic, filtered_slide_titles, slide_contents)
-        # #print("Presentation generated successfully!")
-        st.markdown(get_ppt_download_link(topic), unsafe_allow_html=True)
-
-
-
+    # if GenerateButton and topic:
+    if (GenerateButton or user_input) and topic:
+        # every time user click the button, the local temp file ppt will be removed
+        if st.session_state.generatedpath is not None:
+                os.remove(st.session_state.generatedpath)
+                st.session_state.generatedpath = None
+        if user_input != None:
+                # chat_history contains topic in first line
+                if (st.session_state.chat_history == []) or topic != st.session_state.chat_history[0]:
+                    st.session_state.chat_history = []
+                    st.session_state.chat_history.append(topic)
+                st.session_state.chat_history.append(f"You: {user_input}")
+        # popup window to show the generating status
+        with my_modal.container():
+            with st.spinner("Generating presentation... Please wait."):
+                    #** debug
+                    # time.sleep(2)
+    
+                    # main part to generate the presentation
+                    
+                    # # you should call df, txt, wd, md to generate the presentation
+                    # # chat_history is also need to be called in prompt
+    
+                    # # example
+                    # slide_titles = generate_slide_titles(st.session_state.MinSlide,st.session_state.MaxSlide,topic)
+                    # filtered_slide_titles= [item for item in slide_titles if item.strip() != '']
+                    # #print("Slide Title: ", filtered_slide_titles)
+    
+                    # slide_contents = [generate_slide_content(st.session_state.MaxLine,title) for title in filtered_slide_titles]
+                    # #print("Slide Contents: ", slide_contents)
+                    # create_presentation(topic, filtered_slide_titles, slide_contents)
+                    # #print("Presentation generated successfully!")
+                    # st.markdown(get_ppt_download_link(topic), unsafe_allow_html=True)
+                    
+                    #** debug 
+                    filtered_slide_titles = ['Introduction', 'Methodology', 'Results', 'Conclusion']
+                    slide_contents = ['This is the introduction slide.', 'This is the methodology slide.', 'This is the results slide.', 'This is the conclusion slide.']
+                    st.session_state.outputpath = create_presentation(topic, filtered_slide_titles, slide_contents)
+                    st.session_state.generatedpath = st.session_state.outputpath
+                    st.success("Presentation generated successfully!")
+            if show_output_preview:
+                    st.warning("Generating preview... Please wait.")
+            my_modal.close() # Close the popup window automatically after generating the presentation
+    
 if __name__ == "__main__":
     main()
 
